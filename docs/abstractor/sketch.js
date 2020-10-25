7f589a9df861a5c769736850043e62630cea0432
Sergio Rodríguez Gómez <46205341+srsergiorodriguez@users.noreply.github.com>
2020-10-24 21:20:39 -0500

    Primer commit

---
+++ docs/abstractor/sketch.js
@@ -0,0 +1,208 @@
+/* 
+Abstractor: una aplicación para crear y recuperar figuras abstractas 
+por medio de un código numérico.
+Por Sergio Rodríguez Gómez
+V.2.0.0
+MIT LICENSE
+*/
+
+let codeInput;
+let btnURLS = {shape:[],size:[],position:[],rotation:[]};
+let iconBtns = [];
+let shapeSets = [];
+
+function setup() {
+	createCanvas(res,res).id('canvas').parent(app_canvas_container);
+	background(255);
+	setParameters(); // Change the app config according to url parameters
+	createCodeInput(); // Create the div that will contain the code and its buttons
+	createIconImgs(); // Create images for the buttons that will represent the possible transformations
+	readCode(); // Read the code by dividing it in subcodes and create the corresponding shapes
+	createShapeSets(); // Read the code by dividing it in subcodes
+
+	iconBtns = selectAll(".icon_button");
+	highlightBtns();
+	shapeSets = selectAll(".shape_set");
+}
+
+function setParameters() {
+	let params = getURLParams();
+	if (params.pal) {
+		colors = palettes.hasOwnProperty(params.pal)?palettes[params.pal]:colors;
+	}
+}
+
+function createCodeInput() {
+	// Create the DOM elements for the code input
+	codeInput = createInput(arrayToString(code)).parent(app_code_container).class("code_input");
+	createButton('leer código').parent(app_code_container).class("input_btn")
+		.mouseClicked(submitCode);
+	
+	createButton('código aleatorio').parent(app_code_container).class("input_btn")
+		.mouseClicked(randomCode);
+
+	createButton('guardar imagen').parent(app_code_container).class("input_btn")
+		.mouseClicked(saveImage);
+}
+
+function createShapeSets() {
+	// Create a complete set of controls with multiple sets of buttons
+	for (let i=0;i<shapes;i++) {
+		const shapeSet = createDiv().parent(shape_control).class("shape_set");
+		createElement('p','Figura '+(i+1)).parent(shapeSet);
+		createIconButtonsSet(shapeSet,i);
+	}
+}
+
+function createIconButtonsSet(parent_,shapeSetIndex_) {
+	// Create a set of buttons
+	for (let i=0;i<attributeNr;i++) {
+		createIconButtons(attributeKeys[i],parent_,i+(shapeSetIndex_*attributeNr));
+	}
+}
+
+function createIconButtons(iconType_,parent_,setIndex_) {
+	// Create invididual Buttons
+	const buttonsDiv = createDiv().class("buttons_div").parent(parent_);
+	if (iconType_=="color") {
+		for (let i=0;i<colors.length;i++) {
+			createDiv("").style('background',colors[i]).parent(buttonsDiv).class("icon_button")
+				.mouseClicked(()=>{setCode(setIndex_,i)});
+		}
+	} else {
+		for (let i=0;i<cmax;i++) {
+			createImg(btnURLS[iconType_][i]).parent(buttonsDiv).class("icon_button")
+				.mouseClicked(()=>{setCode(setIndex_,i)});
+		}
+	}
+}
+
+function highlightBtns() {
+	for (let i=0;i<iconBtns.length;i++) {
+		iconBtns[i].removeClass('highlighted_button');
+	}
+	for (let i=0;i<code.length;i++) {
+		iconBtns[i*cmax+(code[i])].addClass('highlighted_button');
+	}
+}
+
+function highlightSet(index_) {
+	unHighlightSet();
+	let tempset = floor(index_/(code.length/shapes));
+	shapeSets[tempset].addClass('highlighted_set');
+}
+
+function unHighlightSet() {
+	for (let i=0;i<shapeSets.length;i++) {
+		shapeSets[i].removeClass('highlighted_set');
+	}
+}
+
+function getImageBtn() {
+	return document.getElementById('canvas').toDataURL('image/png',0.1);
+}
+
+function submitCode() {
+	if (codeInput.value().length===code.length&&!(/[a-z !_]/gi.test(codeInput.value()))) {
+		code = [...stringToArray(codeInput.value())];
+		readCode();
+		unHighlightSet();
+		highlightBtns();
+	} else {
+		codeInput.value('código invalido');
+	}
+}
+
+function resetCode() {
+	code = Array(codeLength).fill(0);
+}
+
+function randomCode() {
+	let randomCode = [];
+	for (let i=0;i<code.length;i++) {
+		let num = int(random(9));
+		randomCode[i] = num;
+	}
+	code=randomCode;
+	readCode();
+}
+
+function saveImage() {
+	saveCanvas("Abstractor"+codeInput.value(),'png');
+}
+
+function arrayToString(arr_) {
+	// Convert an array of numbers to a string
+	return arr_.reduce((a,c)=>a+c,"");
+}
+
+function stringToArray(str_) {
+	// Convert a string to an array of numbers
+	let arr = [];
+	for (let i=0;i<str_.length;i++) {
+		const char = str_.substr(i,1);
+		arr[i] = int(char);
+	}
+	return arr;
+}
+
+function updateCodeString() {
+	// Update the value of the input with the internal code
+	codeInput.value(arrayToString(code));
+}
+
+function setCode(index_,number_) {
+	// Set a particular value in an index of the code
+	code[index_] = number_;
+	readCode();
+	highlightSet(index_);
+	highlightBtns();
+}
+
+function readCode() {
+	// Read the code by dividing it in subcodes
+	background(255);
+	for (let i=0;i<shapes;i++) {
+		const subCode = code.slice(i*attributeNr,i*attributeNr+attributeNr);
+		const shape = createShapeImg(createGraphics(res,res),...subCode);
+		image(shape,0,0);
+	}
+	updateCodeString();
+}
+
+function createIconImgs() {
+	//shape,size,color,position,rotation
+	resetCode();
+	for (let i=0;i<cmax;i++) {
+	  // Shape
+	  code[0] = i;
+	  code[1] = 7;
+	  code[2] = 1;
+	  code[3] = 5;
+	  code[4] = 0;
+	  readCode();
+	  btnURLS.shape[i] = getImageBtn();
+		// Size
+		code[0] = 4;
+		code[1] = i;
+		readCode();
+		btnURLS.size[i] = getImageBtn();
+		// Position
+		code[1] = 3;
+		code[2] = 2;
+		code[3] = i;
+		code = i===0?[...[6,9,2,5,3,6,9,2,5,5],...Array(codeLength-10).fill(0)]:code; // Create an 'X' to represent the absence of position
+		readCode();
+		btnURLS.position[i] = getImageBtn();
+		// Rotation
+		resetCode();
+		code[0] = 8;
+		code[1] = 5;
+		code[2] = 1;
+		code[3] = 5;
+		code[4] = i;
+		readCode();
+		btnURLS.rotation[i] = getImageBtn();
+	}
+	resetCode();
+}
---
+++ docs/core.js
@@ -0,0 +1,92 @@
+const res = 450; // Default size of the canvas
+const attributeKeys = ['shape','size','color','position','rotation'];
+const shapes = 3; // Default number of shapes
+const attributeNr = attributeKeys.length;
+const codeLength = attributeNr*shapes;
+const codesNum = 4; // Number of concatenated codes in compositor
+const cmax = 10; // Base for the code
+let code = Array(codeLength).fill(0); // Initial state of the code
+let colors = palettes["basic"];
+const rotationAngles = [0,45,90,135,180,225,270,315,45/2,225/2];
+
+function createShapeImg(img,shape_,size_,color_,position_,rotation_) {
+	// Create a shape based on a subcode
+	if (position_==0) {
+	} else {
+		img.push();
+		angleMode(DEGREES);
+		//Define position
+		let pos = position_===0?{}:{
+			x:res/6*((Math.floor((position_-1)/3)*2)+1),
+			y:res/6*((((position_-1)%3)*2)+1)
+		};
+		img.translate(pos.x,pos.y);
+
+		//Define rotation
+		if (rotation_<8) {
+			img.rotate(rotationAngles[rotation_]);
+		} else if (rotation_==8) {
+			img.applyMatrix(-1,0,0,1,0,0);
+		} else {
+			img.applyMatrix(0,1,1,0,0,0);
+		}
+		
+		//Define size
+		size = map(size_,0,9,res/10,res);
+
+		// Define color
+		img.noStroke();
+		c = color(colors[color_]);
+		img.fill(c);
+
+		// Define shape
+		if (shape_==0) {
+			//Circle
+			img.ellipse(0,0,size,size);
+		} else if (shape_==1) {
+			//HalfCircle
+			img.beginShape()
+			for (let i=90;i<=270;i++) {
+				let x = size/2*cos(i);
+				let y = size/2*sin(i);
+				img.vertex(x,y);
+			}
+			img.endShape();
+		} else if (shape_==2) {
+			//Ellipse
+			img.ellipse(0,0,size/2,size);
+		} else if (shape_==3) {
+			//HalfEllipse
+			img.beginShape()
+			for (let i=90;i<=270;i++) {
+				let x = size/4*cos(i);
+				let y = size/2*sin(i);
+				img.vertex(x,y)
+			}
+			img.endShape();
+		} else if (shape_==4) {
+			//Square
+			img.rect(-(size/2),-(size/2), size, size);
+		} else if (shape_==5) {
+			//Rectangle
+			img.rect(-((size/2)/2),-(size/2), size/2, size);
+		} else if (shape_==6) {
+			//Line
+			img.rect(-((size/2)/8),-(size/2), size/8, size);
+		} else if (shape_==7) {
+			//Triangle
+			img.triangle(-(size/2),(size/2),(size/2),(size/2),0,-(size/2));
+		} else if (shape_==8) {
+			//HalfTriangleLeft
+			img.triangle(-(size/2),(size/2),0,(size/2),0,-(size/2));
+		} else if (shape_==9) {
+			//Triangle2
+			img.triangle(-(size/2),(size/2),(size/2),(size/2),(size/2),-(size/2));
+		} else {
+		}
+		img.pop();
+	}
+	return img;
+}
+
+// 000000000016450
